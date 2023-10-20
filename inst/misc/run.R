220b1dc00a6deeabc2e4cbba8781c52b35c5a4a5
emilymarkowitz-noaa <emily.markowitz@noaa.gov>
2023-10-19 21:03:46 -0700

    added improved and new FOSS data

--- inst/misc/run.R
+++ inst/misc/run.R
@@ -1,5 +1,195 @@
 
 
+# Load new FOSS data -----------------------------------------------------------
+
+# This has a specific username and password because I DONT want people to have access to this!
+# source("https://raw.githubusercontent.com/afsc-gap-products/metadata/main/code/functions_oracle.R")
+
+library(magrittr)
+library(readr)
+library(dplyr)
+
+locations <- c("Z:/Projects/ConnectToOracle.R")
+for (i in 1:length(locations)){
+  if (file.exists(locations[i])){
+    source(locations[i])
+  }
+}
+
+metadata_table_comment <- dplyr::bind_rows(
+  # tables
+  RODBC::sqlQuery(
+    channel = channel_products,
+    query = "SELECT table_name, comments
+FROM all_tab_comments
+WHERE owner = 'GAP_PRODUCTS'
+ORDER BY table_name") %>% 
+    data.frame(), 
+  # materialized view
+  RODBC::sqlQuery(
+    channel = channel_products,
+    query = "SELECT *FROM user_mview_comments") %>% 
+    data.frame() %>% 
+    dplyr::rename(TABLE_NAME = MVIEW_NAME)
+) %>% 
+  dplyr::filter(TABLE_NAME == "FOSS_CATCH")
+
+metadata_table_comment <- paste0(metadata_table_comment$COMMENTS, 
+                                 " Data was pulled ", Sys.Date(), ". ")
+
+metadata_colname <- RODBC::sqlQuery(
+  channel = channel_products, 
+  query = "SELECT * FROM GAP_PRODUCTS.METADATA_COLUMN") %>% 
+  janitor::clean_names()
+
+noaa_afsc_public_foss <- RODBC::sqlQuery(
+    channel = channel_products, 
+    query = 
+      "SELECT DISTINCT 
+hh.YEAR,
+hh.SRVY,                 
+hh.SURVEY,
+hh.SURVEY_DEFINITION_ID,
+hh.SURVEY_NAME,
+hh.CRUISE,
+hh.CRUISEJOIN,           
+hh.HAUL,
+hh.HAULJOIN,
+hh.STRATUM,
+hh.STATION,
+hh.VESSEL_ID,
+hh.VESSEL_NAME,          
+hh.DATE_TIME,
+hh.LATITUDE_DD_START, 
+hh.LONGITUDE_DD_START, 
+hh.LATITUDE_DD_END,
+hh.LONGITUDE_DD_END, 
+hh.BOTTOM_TEMPERATURE_C,
+hh.SURFACE_TEMPERATURE_C,
+hh.DEPTH_M,
+cc.SPECIES_CODE,
+ss.ITIS,
+ss.WORMS,
+ss.COMMON_NAME,     
+ss.SCIENTIFIC_NAME,
+ss.ID_RANK,
+cc.TAXON_CONFIDENCE,
+cc.WEIGHT_KG,
+cc.COUNT,
+cc.CPUE_KGKM2,
+cc.CPUE_NOKM2,
+hh.AREA_SWEPT_KM2,       
+hh.DISTANCE_FISHED_KM,
+hh.DURATION_HR,          
+hh.NET_WIDTH_M,
+hh.NET_HEIGHT_M,
+hh.PERFORMANCE 
+FROM GAP_PRODUCTS.FOSS_CATCH cc
+LEFT JOIN GAP_PRODUCTS.FOSS_HAUL hh
+ON cc.HAULJOIN = hh.HAULJOIN
+FULL JOIN GAP_PRODUCTS.FOSS_SPECIES_TEST ss
+ON cc.SPECIES_CODE = ss.SPECIES_CODE
+WHERE hh.SRVY = 'EBS' 
+AND cc.SPECIES_CODE IN (21740, 10210, 69323) 
+AND hh.YEAR >= 2015") %>% 
+  janitor::clean_names()
+
+# Save table to local directory
+save(noaa_afsc_public_foss, file = here::here("data", "noaa_afsc_public_foss.rda"))
+  
+column <- metadata_colname %>%
+  dplyr::filter(metadata_colname %in% toupper(names(noaa_afsc_public_foss))) %>%
+  dplyr::mutate(metadata_colname = tolower(metadata_colname)) %>%
+  dplyr::distinct()
+
+# column <- read_csv(file = "../notforgit/metadata_column_current.csv")
+# column <- column[column$TABLE_NAME == "FOSS_CPUE_PRESONLY",]
+# column$col_name <- names(public_data)
+# table <- read_csv(file = "../notforgit/metadata_table_current.csv")
+
+table0 <- "noaa_afsc_public_foss"
+
+str0 <- paste0("#' @title Public data from FOSS
+#' @description ",metadata_table_comment," 
+#' @usage data('noaa_afsc_public_foss')
+#' @author Emily Markowitz (Emily.Markowitz AT noaa.gov)
+#' @format A data frame with ",nrow(noaa_afsc_public_foss)," observations on the following ",
+ncol(noaa_afsc_public_foss)," variables.
+#' \\describe{
+",
+               paste0(paste0("#'   \\item{\\code{",column$metadata_colname,"}}{", column$metadata_colname_long, ". ", column$metadata_colname_desc,"}"), collapse = "\n"),
+               "#'   }
+#' @source https://github.com/afsc-gap-products/gap_public_data
+#' @keywords species code data
+#' @examples
+#' data(noaa_afsc_public_foss)
+#' @details DETAILS
+'noaa_afsc_public_foss'")
+
+write.table(str0, 
+            file = here::here("R","noaa_afsc_public_foss.R"), 
+            sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+
+## Station centroid data -------------------------------------------------------
+
+library(akgfmaps)
+
+sel_region <- c("ai", "goa", "ebs", "nbs")
+stn_col <- c("ID", "ID", "STATIONID", "STATIONID")
+
+station_coords <- data.frame()
+
+for(ii in 1:length(sel_region)) {
+  map_layers <- akgfmaps::get_base_layers(select.region = sel_region[ii], set.crs = "EPSG:4326")
+  
+  station_center <- map_layers$survey.grid |>
+    sf::st_make_valid() |>
+    sf::st_centroid()
+  
+  station_center <- data.frame(station = station_center[[stn_col[ii]]]) |>
+    dplyr::bind_cols(sf::st_coordinates(station_center)) |>
+    dplyr::rename(longitude_dd = X, latitude_dd = Y) |>
+    dplyr::mutate(srvy = toupper(sel_region[ii]))
+  
+  station_coords <- station_coords |>
+    dplyr::bind_rows(station_center)
+}
+
+station_coords <- station_coords %>%
+  dplyr::filter(!is.na(srvy)) %>%
+  dplyr::filter(!is.na(station)) %>%
+  dplyr::filter(!is.na(longitude_dd)) %>%
+  dplyr::filter(!is.na(latitude_dd))
+
+save(station_coords, file = "./data/station_coords.rda")
+
+column <- gap_products_metadata_column0 %>%
+  dplyr::filter(metadata_colname %in% toupper(names(station_coords))) %>%
+  dplyr::mutate(metadata_colname = tolower(metadata_colname)) %>%
+  dplyr::distinct()
+
+table <- "Station centroid coordinates for each station for all surveys, as defined by the {akgfmaps} package. "
+
+str0 <- paste0("#' @title Station centroid locations for each station from akgfmaps
+#' @description ",table,"
+#' @usage data('station_coords')
+#' @author Sean Rohan (sean.rohan AT noaa.gov)
+#' @format A data frame with ",nrow(station_coords)," observations on the following ",ncol(station_coords)," variables.
+#' \\describe{
+",
+               paste0(paste0("#'   \\item{\\code{",column$metadata_colname,"}}{", column$metadata_colname_long, ". ", column$metadata_colname_desc,"}"), collapse = "\n"),
+               "#'   }
+#' @source https://github.com/afsc-gap-products/akgfmaps
+#' @keywords station survey data
+#' @examples
+#' data(station_coords)
+#' @details DETAILS
+'station_coords'")
+
+write.table(str0, file = "./R/station_coords.R", sep = "\t",
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
+
 # Document and create Package --------------------------------------------------
 .rs.restartR()
 
@@ -17,16 +207,12 @@ install("sdmgamindex")
 setwd(here::here())
 # devtools::check()
 
-# Create Package Readme.md -----------------------------------------------------
+## Create Package Readme.md -----------------------------------------------------
 rmarkdown::render(here::here("inst", "misc", "README.Rmd"),
                   output_dir = "./",
                   output_file = "README.md")
 
-# Add stuff --------------------------------------------------------------------
-
-# usethis::use_vignette("C-model-comparisons")
-
-# Create Documentation GitHub-Pages --------------------------------------------
+## Create Documentation GitHub-Pages -------------------------------------------
 
 .rs.restartR()
 # devtools::install_github("rstudio/fontawesome", force = T)
@@ -34,25 +220,15 @@ rmarkdown::render(here::here("inst", "misc", "README.Rmd"),
 library(here)
 library(usethis)
 library(pkgdown)
-
-# use_logo(here::here("notforgit","logo.png"))
-
-# Run once to configure package to use pkgdown
-# usethis::use_pkgdown()
-# Run to build the website
-options(rmarkdown.html_vignette.check_title = FALSE)
-pkgdown::build_site()
+rmarkdown::render(input = "./inst/r/README.Rmd",
+                  output_dir = "./",
+                  output_file = "README.md")
 
 
-# If you’re using GitHub, we also recommend setting up GitHub actions to automatically build and publish your site:
-# usethis::use_pkgdown_github_pages()
+# devtools::install_github("r-lib/pkgdown")
+# pkgdown::build_favicons()
+# devtools::build_vignettes()
+# usethis::use_pkgdown(config_file = "./pkgdown/_pkgdown.yml")
 
-# Save Package tar.gz
-version0 <- "0.1.0"
-file.remove(paste0(dirname(here::here()), "/sdmgamindex_",version0,".tar.gz"))
-file.remove(paste0((here::here()), "/sdmgamindex_",version0,".tar.gz"))
-devtools::build()
-file.copy(from = paste0(dirname(here::here()), "/sdmgamindex_",version0,".tar.gz"),
-          to = paste0(here::here(), "/sdmgamindex_",version0,".tar.gz"),
-          overwrite = TRUE)
-
+pkgdown::build_site(pkg = here::here())
+# usethis::use_github_action("pkgdown")
