ab6abac3fb94f723cda50b563e491211d7691a01
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-08-28 16:55:31 -0700

    add pred_grid_goa to package data

---
+++ R/pred_grid_goa.R
@@ -0,0 +1,16 @@
+#' GOA Prediction Grid
+#'
+#' Prediction (extrapolation) grid for GOA survey data.
+#'
+#' @format A data frame with 22604 rows and 3 variables:
+#' \describe{
+#'   \item{lon}{Longitude}
+#'   \item{lat}{Latitude}
+#'   \item{Shape_Area}{Grid cell area}
+#'   ...
+#' }
+#' @source GOA prediction grid used for index production.
+#' @examples
+#' data(pred_grid_goa)
+#' head(pred_grid_goa)
+"pred_grid_goa"
